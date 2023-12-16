3bdcf2ec4d42ce689c174ad4f303ac892c89b982
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-12-16 18:23:32 +0300

    Configure allowed hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-nn1f#e5kc3j35el!c*0t@gq)bz+dlj7bep(u)($(zc_ea980gg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
