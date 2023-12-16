a2c9005aaef0fda9f75d219d6a80249ad7537576
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-12-16 20:47:03 +0300

    Add configuration files for vercel

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -127,8 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-STATIC_ROOT = BASE_DIR / 'static_root'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
