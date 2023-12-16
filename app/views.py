8aed5baa054132486180c116977f1582f267edc4
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-12-16 17:48:10 +0300

    Handle sending mails

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage
+from django.contrib import messages
 
 
 def home(request):
@@ -6,4 +8,22 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        # Replace with the recipient's email address
+        recipient_email = 'wamaejoseph392@gmail.com'
+        emailObj = EmailMessage(
+            f'{subject}',
+            f'Name: {name}\nEmail: {email}\nMessage: {message}',
+            email,
+            [recipient_email],
+            reply_to=[email],
+        )
+        emailObj.send()
+        messages.success(request, 'Your message was sent successfully!')
+        return redirect('contacts')
     return render(request, 'contact.html')
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -101,6 +101,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Email configurations
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'v0793844124@gmail.com'
+EMAIL_HOST_PASSWORD = 'aixv rlzv wktj wzwn'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
