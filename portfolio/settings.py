68e2edb334a72a7ed3a2ceaf3499712fbf301083
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-12-16 21:34:46 +0300

    Configure to a dummy db

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,8 +77,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy',
     }
 }
 
